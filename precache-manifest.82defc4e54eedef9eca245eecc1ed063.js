d2495d324a14444b35417237701694f568ee51f9
gbrquest <gbrquest@gmail.com>
2023-09-03 14:04:46 +0530

    Updates

---
+++ precache-manifest.82defc4e54eedef9eca245eecc1ed063.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfae25e7c5a8dced2ba8",
+    "url": "/findInsight/static/css/main.2a4959f1.chunk.css"
+  },
+  {
+    "revision": "cfae25e7c5a8dced2ba8",
+    "url": "/findInsight/static/js/main.fccaac5f.chunk.js"
+  },
+  {
+    "revision": "2f01552c4b6320f2300a",
+    "url": "/findInsight/static/js/runtime~main.4e244f07.js"
+  },
+  {
+    "revision": "07ebe8083fdceb3daa2d",
+    "url": "/findInsight/static/css/2.0c81adc3.chunk.css"
+  },
+  {
+    "revision": "07ebe8083fdceb3daa2d",
+    "url": "/findInsight/static/js/2.5b265c29.chunk.js"
+  },
+  {
+    "revision": "733e3c73640bd34b9497d6badc391636",
+    "url": "/findInsight/index.html"
+  }
+];
